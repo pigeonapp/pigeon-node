b366ceaed7e3504e47a08ec36fd64de051277dbb
Rajat Sharma <lunasunkaiser@gmail.com>
2019-05-04 15:31:11 +0530

    Passing: Should send mail without attachments

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,6 +3,11 @@ import pigeon from '../src/index';
 
 const Pigeon = pigeon({ publicKey: '55oepunmrLU8DCbcPsjCrkdf', privateKey: 'VTpFahdeEP3d1eBtJKFjXCX2nk2t4knG' });
 
+process.env.API_URL = 'https://pigeon-api-staging.herokuapp.com/v1/deliveries';
+
 test('Should send mail without attachments', async t => {
-  await Pigeon.deliver('confirmation-mail', { to: 'lunasunkaiser@gmail.com' }).then(_ => t.pass());
+  await Pigeon.deliver('confirmation-mail', { to: 'rajat@keepworks.com' }).then(result => {
+    console.log(result.body);
+    t.pass();
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const http = require('got');
 
-const API_URL = 'http://api.pigeon-staging.herokuapp.com/v1/deliveries';
-
 module.exports = ({ publicKey, privateKey }) => {
   function deliver(messageIdentifier, parcels) {
-    return http.post(API_URL, {
-      headers: { 'X-Public-Key': publicKey, 'X-Private-Key': privateKey },
+    return http.post(process.env.API_URL || 'https://api.pigeonapp.io/v1/deliveries', {
+      headers: {
+        'user-agent': 'pigeon-node-sdk',
+        'X-Public-Key': publicKey,
+        'X-Private-Key': privateKey,
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({
         message_identifier: messageIdentifier,
         parcels,
