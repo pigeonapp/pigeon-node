b2b5e0aef7562ff2d88f9dbc135c7339a0d5b8ef
Rajat Sharma <lunasunkaiser@gmail.com>
2019-05-06 08:23:01 +0530

    Seperated Client class

---
+++ src/Client.js
@@ -0,0 +1,55 @@
+const http = require('got');
+const fs = require('fs');
+
+const readFilePromised = attachment =>
+  new Promise((resolve, reject) => {
+    fs.readFile(attachment.file, (err, data) => {
+      if (err) reject(err);
+      resolve({
+        content: Buffer.from(data).toString('base64'),
+        name: attachment.name,
+      });
+    });
+  });
+
+const processAttachments = parcel => {
+  if (!parcel.attachments) {
+    return Promise.resolve(parcel);
+  }
+
+  return Promise.all(parcel.attachments.map(readFilePromised)).then(processedAttachments =>
+    Object.assign({}, parcel, {
+      attachments: processedAttachments,
+    }),
+  );
+};
+
+module.exports = class Client {
+  constructor({
+    baseUri = process.env.PIGEON_BASE_URI || 'https://api.pigeonapp.io/v1',
+    publicKey = process.env.PIGEON_PUBLIC_KEY,
+    privateKey = process.env.PIGEON_PRIVATE_KEY,
+  } = {}) {
+    this.baseUri = baseUri;
+    this.publicKey = publicKey;
+    this.privateKey = privateKey;
+  }
+
+  deliver(messageIdentifier, parcels) {
+    const processedParcels = Array.isArray(parcels) ? parcels : [parcels];
+    return Promise.all(processedParcels.map(parcel => processAttachments(parcel))).then(finalParcels =>
+      http.post(`${this.baseUri}/deliveries`, {
+        headers: {
+          'User-Agent': 'pigeon-node',
+          'X-Public-Key': this.publicKey,
+          'X-Private-Key': this.privateKey,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message_identifier: messageIdentifier,
+          parcels: finalParcels,
+        }),
+      }),
+    );
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,49 +1,6 @@
-const http = require('got');
-const fs = require('fs');
+const Client = require('./Client');
 
-const readFilePromised = attachment =>
-  new Promise((resolve, reject) => {
-    fs.readFile(attachment.file, (err, data) => {
-      if (err) reject(err);
-      resolve({
-        content: Buffer.from(data).toString('base64'),
-        name: attachment.name,
-      });
-    });
-  });
-
-const processAttachments = parcel =>
-  Promise.all(parcel.attachments.map(readFilePromised)).then(processedAttachments =>
-    Object.assign({}, parcel, {
-      attachments: processedAttachments,
-    }),
-  );
-
-const createClient = ({
-  baseUri = process.env.PIGEON_BASE_URI || 'https://api.pigeonapp.io/v1',
-  publicKey = process.env.PIGEON_PUBLIC_KEY,
-  privateKey = process.env.PIGEON_PRIVATE_KEY,
-} = {}) => {
-  function deliver(messageIdentifier, parcels) {
-    const processedParcels = Array.isArray(parcels) ? parcels : [parcels];
-    return Promise.all(processedParcels.map(parcel => processAttachments(parcel))).then(finalParcels =>
-      http.post(`${baseUri}/deliveries`, {
-        headers: {
-          'User-Agent': 'pigeon-node',
-          'X-Public-Key': publicKey,
-          'X-Private-Key': privateKey,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          message_identifier: messageIdentifier,
-          parcels: finalParcels,
-        }),
-      }),
-    );
-  }
-
-  return { deliver };
-};
+const createClient = params => new Client(params);
 
 module.exports = {
   createClient,
